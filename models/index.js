3db591d6e52b4c7ea3b3762c85207a9d17582c10
Simon Brown <simonandon@live.com.au>
2024-05-13 11:13:46 +0800

    Set up many-to-many association for Traveller and Location through Trip

--- models/index.js
+++ models/index.js
@@ -2,3 +2,11 @@ const Traveller = require('./Traveller');
 const Location = require('./Location');
 const Trip = require('./Trip');
 
+Traveller.belongsToMany(Location, { through: Trip });
+Location.belongsToMany(Traveller, { through: Trip });
+
+module.exports = {
+	Traveller,
+	Location,
+	Trip,
+};
