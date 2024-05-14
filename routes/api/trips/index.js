c157d7a65b46e3b3d7a55989a30feec6053b633f
Simon Brown <simonandon@live.com.au>
2024-05-14 11:03:06 +0800

    Added post route to add new trip

--- routes/api/trips/index.js
+++ routes/api/trips/index.js
@@ -1,6 +1,20 @@
 const router = require('express').Router();
 const { Traveller, Location, Trip } = require('../../../models');
 
-// routes
+// create new trip route
+router.post('/', async (req, res) => {
+	try {
+		const { trip_budget, traveller_amount, traveller_id, location_id } = req.body;
+		const createTrip = await Trip.create({
+			trip_budget,
+			traveller_amount,
+			traveller_id,
+			location_id
+		});
+		res.status(200).json(createTrip);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
 module.exports = router;
