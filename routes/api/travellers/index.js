f81586087063e24db9becc592ba87b3287d1766d
Simon Brown <simonandon@live.com.au>
2024-05-14 10:17:12 +0800

    Added route to get all trip data associated with a provided traveller id

--- routes/api/travellers/index.js
+++ routes/api/travellers/index.js
@@ -34,4 +34,33 @@ router.post('/', async (req, res) => {
 	}
 });
 
+// get a single traveller's trip data
+router.get('/:id', async (req, res) => {
+	try {
+		const travellerId = req.params.id;
+	
+		const travellerData = await Traveller.findByPk(travellerId);
+
+		if (travellerData) {
+			const tripData = await Trip.findAll({
+				where: {
+					traveller_id: travellerId
+				}
+			});
+
+			if (tripData) {
+				res.status(200).json(tripData);
+			} else {
+				res.status(404).json({message: 'Traveller has no associated Trips'});
+			}
+		} else {
+			res.status(404).json({message: 'No Traveller with this id'});
+		}
+	
+		
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
 module.exports = router;
