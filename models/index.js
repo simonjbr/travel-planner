5ea709c2da90e06344b276531b1f63ca3eded9ba
Simon Brown <simonandon@live.com.au>
2024-05-14 09:06:25 +0800

    Passed unique: false in the options object of both belongsToMany calls to allow non-unique composite keys

--- models/index.js
+++ models/index.js
@@ -2,8 +2,8 @@ const Traveller = require('./Traveller');
 const Location = require('./Location');
 const Trip = require('./Trip');
 
-Traveller.belongsToMany(Location, { through: Trip });
-Location.belongsToMany(Traveller, { through: Trip });
+Traveller.belongsToMany(Location, { through: { model: Trip, unique: false } });
+Location.belongsToMany(Traveller, { through: { model: Trip, unique: false } });
 
 module.exports = {
 	Traveller,
